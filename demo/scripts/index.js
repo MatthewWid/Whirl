73721320aa247f0750487a85a8a6f9aa052215d0
Matthew Widdicombe <115mattey@gmail.com>
2018-08-22 14:19:18 +1000

    Event system and standard object instantiation - Added stdObj for a standard instantiation of all game objects with an optional attachment of an event and child system - Updated event system to be an array on each event that is called when an event is invoked. - Added simple image asset preparation and preloading. - Updated naming of the asset, stage and viewport manager

--- demo/scripts/index.js
+++ demo/scripts/index.js
@@ -1,9 +1,33 @@
 let game = new MobSin();
 
-game.viewport.add("mainCanvas", "#canvas", game.stage.add("world"), {
-	width: 480,
-	height: 280,
-	bg: "rgb(0, 0, 150)"
+let startInd = game.assetM.add([
+	{
+		name: "zombie",
+		type: "image",
+		src: "./img/zombiehead.png"
+	},
+	{
+		name: "player",
+		type: "image",
+		src: "./img/playerSprite.png"
+	},
+	{
+		name: "giant_image",
+		type: "image",
+		src: "http://s1.1zoom.me/big3/189/Planets_solar_system_449630.jpg"
+	}
+]);
+
+for (let i = startInd; i < game.assetM.getAll().length; i++) {
+	console.log("Starting: " + game.assetM.getAll()[i]._id);
+	game.assetM.getAll()[i].event.listener("didLoad", () => {
+		console.log("Loaded: " + game.assetM.getAll()[i]._id);
+	});
+}
+
+game.viewportM.add("mainC", "#canvas", game.stageM.add("world"), {
+	cW: 500,
+	cH: 400
 });
 
 game.start();
--- src/MobSin.js
+++ src/MobSin.js
@@ -1,46 +1,148 @@
 function MobSin() {
 	// The global index of all objects in the game
-	// Assigns unique IDs to object
+	// Assigns unique IDs to objects
 	let globalIndex = 0;
 
 	// If the game is playing or not
 	this.playing = false;
+	// The current frame count - How many times the game has updated
 	this.frameCount = 0;
 
-	// Viewports are canvasses that the game can render to
-	// You can have multiple viewports associated with different canvasses and stages
-	this.viewports = [];
+	// All game objects will "extend" this object so that they have these methods
+	// Objects inheriting this can choose to inheret an event system and a child system as well via the cfg parameter
+	// changables is a list of properties of the object that are allowed to be manipulated from outside the context of the object
+	let stdObj = (that, cfg = {}, changables = []) => {
+		that._id = globalIndex++;
+		that._type = cfg.typeName ? cfg.typeName : "";
+		that.data = {};
+
+		that.set = (newCfg) => {
+			for (let i = 0, n = changables.length; i < n; i++) {
+				that[changables[i]] = newCfg[changables[i]] ? newCfg[changables[i]] : that[changables[i]];
+			}
+		}
+
+		// Object event manager
+		if (cfg.eventSystem) {
+			that.events = {};
+			that.event = that.evt = {
+				create: (name, func) => {
+					that.events[name] = func ? [func] : [];
+				},
+				listener: (name, func) => {
+					that.events[name].push(func);
+				},
+				// TODO: Ability to remove events
+				invoke: (name, params) => {
+					if (that.events[name]) {
+						for (let i = 0, n = that.events[name].length; i < n; i++) {
+							that.events[name][i](params);
+						}
+						return true;
+					}
+					return false;
+				}
+			};
+		}
+
+		// Object child manager
+		if (cfg.childSystem) {
+			that.children = [];
+			// Object children manager
+			that.addChild = (newChild) => {
+				if (typeof newChild === "object" && !Array.isArray(newChild)) {
+					that.children.push(newChild);
+				} else if (Array.isArray(newChild)) {
+					for (let i = 0, n = newChild.length; i < n; i++) {
+						that.addChild(newChild[i]);
+					}
+				}
+			};
+			that.getChildren = () => {
+				return that.children;
+			};
+		}
+	};
+	this.initObj = (that, cfg) => stdObj(that, cfg);
+
+	this.assets = [];
+
+	function asset(name, type, src) {
+		stdObj(this, {
+			typeName: "MobSin.asset",
+			eventSystem: true
+		}, [
+			"name",
+			"type"
+		]);
+
+		this.name = name;
+		this.type = type;
+		this.src = src;
+
+		console.log(this);
+		this.loaded = false;
+		this.event.create("didLoad");
+
+		if (this.type == "image") {
+			this.img = new Image();
+			this.img.onload = () => {
+				this.loaded = true;
+				this.event.invoke("didLoad");
+			};
+			this.img.src = this.src;
+		}
+	}
+
+	// Asset Manager
+	// this.a
+	this.assetM = this.a = {
+		add: (assetList) => {
+			if (typeof assetList == "object" && !Array.isArray(assetList)) {
+				let newInd = this.assets.push(
+					new asset(assetList.name, assetList.type, assetList.src)
+				) - 1;
+				return this.assets[newInd];
+			} else if (Array.isArray(assetList)) {
+				for (let i = 0, n = assetList.length; i < assetList.length; i++) {
+					this.assetM.add(assetList[i]);
+				}
+			}
+			// Return the starting index of the assets array where the new asstes were inserted
+			return this.assets.length - assetList.length;
+		},
+		get: (name) => {
+			return this.assets.find((e) => e.name = name);
+		},
+		getAll: () => {
+			return this.assets;
+		}
+	};
 
 	// Stages are like levels of your game
-	// Each stage is a world that holds objects
+	// Each stage is a separate world that holds objects
 	this.stages = [];
 
-	// Stage object
+	// Stage object definition
 	function stage(name, childList = [], cfg = {}) {
 		// Auto-generated
-		this._id = globalIndex++;
-		this._type = "MobSin.stage";
+		stdObj(this, {
+			typeName: "MobSin.stage",
+			eventSystem: true,
+			childSystem: true
+		}, [
+			"name",
+			"children"
+		]);
 
 		// Mandatory presets
 		this.name = name;
-		let children = [];
-
-		// Methods
-		this.addChild = (newChild) => {
-			if (typeof newChild === "object") {
-				children.push(obj);
-			} else if (newChild instanceof Array) {
-				children.concat(newChild);
-			}
-		};
-		this.getChildren = () => {
-			return children;
-		};
+		this.children = childList;
 	}
 
 	// Stage Manager
 	// this.s
-	this.stage = this.s = {
+	this.stageM = this.s = {
 		add: (name, childList = []) => {
 			let newInd = this.stages.push(
 				new stage(name, childList)
@@ -52,49 +154,58 @@ function MobSin() {
 		},
 		getAll: () => {
 			return this.stages;
-		},
-		remove: (name) => {
-			// TODO: Use destroy methods and call callback once done
-			this.stages = this.stages.filter((e) => e.name != name);
 		}
 	};
 
-	// Viewport object
-	function viewport(name, canvas, stage = {}, cfg = {}) {
-		let c = document.querySelector(canvas);
-		let ctx = c.getContext("2d");
-		c.width = cfg.width ? cfg.width : c.width;
-		c.height = cfg.height ? cfg.height : c.height;
+	// Viewports are canvasses that the game can render to
+	// You can have multiple viewports associated with different canvasses and stages
+	this.viewports = [];
 
+	// Viewport object definition
+	function viewport(name, canvas, stage = {}, cfg = {}) {
 		// Auto-generated
-		this._id = globalIndex++;
-		this._type = "MobSin.viewport";
+		stdObj(this, {
+			typeName: "MobSin.viewport",
+			eventSystem: true
+		}, [
+			"name",
+			"activeStage",
+			"renderable",
+			"clear",
+			"x",
+			"y",
+			"w",
+			"h"
+		]);
 
 		// Mandatory presets
 		this.name = name;
-		this.c = document.querySelector(canvas);
-		this.ctx = ctx;
 		this.activeStage = stage;
 
-		// Optional presets
+		this.renderTo = (newCanvas) => {
+			this.c = document.querySelector(newCanvas);
+			this.ctx = this.c.getContext("2d");
+		};
+		this.renderTo(canvas);
+
+		// Optional presets and defaults
+		this.c.width = cfg.cW ? cfg.cW : this.c.width;
+		this.c.height = cfg.cH ? cfg.cH : this.c.height;
 		this.renderable = cfg.renderable ? cfg.renderable : true;
 		this.clear = cfg.clear ? cfg.clear : true;
-		this.bg = cfg.bg ? cfg.bg : undefined;
+		this.x = cfg.x ? cfg.x : 0;
+		this.y = cfg.y ? cfg.y : 0;
+		this.w = cfg.w ? cfg.w : this.c.width;
+		this.h = cfg.h ? cfg.h : this.c.height;
 
-		// TODO: Add .set({}) to set multiple configuration variables at once
-
-		// Methods
 		this.setStage = () => {
 			this.activeStage = stage;
-		};
-		this.removeStage = () => {
-			this.activeStage = undefined;
 		};
 	}
 
 	// Viewport Manager
 	// this.vp
-	this.viewport = this.vp = {
+	this.viewportM = this.vp = {
 		add: (name, canvas, stage = {}, cfg) => {
 			let newInd = this.viewports.push(
 				new viewport(name, canvas, stage, cfg)
@@ -106,42 +217,64 @@ function MobSin() {
 		},
 		getAll: () => {
 			return this.viewports;
-		},
-		remove: (name) => {
-			this.viewports = this.viewports.filter((e) => e.name != name);
 		}
 	};
 
+	// Give the game container standard objects properties
+	stdObj(this, {
+		typeName: "MobSin.gameContainer",
+		eventSystem: true
+	});
+
+	// Create events for standard game events
+	this.event.create("willUpdate"); // Before any updates are run
+	this.event.create("didUpdate"); // After updates are run
+
+	this.event.create("willRender"); // Before rendering is done
+	this.event.create("didRender"); // After rendering is done
+
+	this.event.create("didStart"); // After the game is started
+	this.event.create("didStop"); // After the game is stopped
+
+	// Start/stop - Resume/pause the game
 	this.start = () => {
-		// TODO: Start timers, tickers and interpolations
 		this.playing = true;
+		this.event.invoke("didStart");
 		update();
 	};
 	this.stop = () => {
-		// TODO: Stop timers, tickers and interpolations
 		this.playing = false;
+		this.event.invoke("didStop");
 	};
 
 	let update = () => {
+		this.event.invoke("willUpdate");
+
+		// Run physics, collisions, animations, renders, etc.
+		render();
+
+		this.event.invoke("didUpdate");
 		if (this.playing) {
-			render();
 			this.frameCount++;
 			requestAnimationFrame(update);
 		}
 	};
 
 	let render = () => {
+		this.event.invoke("willRender");
 		for (let i = 0, n = this.viewports.length; i < n; i++) {
 			let vp = this.viewports[i];
 			if (vp.renderable) {
 				if (vp.clear) {
-					vp.ctx.clearRect(0, 0, vp.c.width, vp.c.height);
+					vp.ctx.clearRect(vp.x, vp.y, vp.w, vp.h);
 				}
 				if (vp.bg) {
 					vp.ctx.fillStyle = vp.bg;
-					vp.ctx.fillRect(0, 0, vp.c.width, vp.c.height);
+					vp.ctx.fillRect(vp.x, vp.y, vp.w, vp.h);
 				}
 			}
+			// this.viewports[i].render();
 		}
+		this.event.invoke("didRender");
 	};
 }
