e60c389c031c9bc18caa8f4e5f4085d66776cb6e
MatthewWid <115mattey@gmail.com>
2018-11-10 12:47:29 +1100

    Added basic Text sprite object.

--- src/text/index.js
+++ src/text/index.js
@@ -1,5 +1,46 @@
 // MobSin.Text
 
-module.exports = () => {
-	return "Text -> index.js";
-};
+/*
+	A text object used to render text in the world.
+	Text is not bound or affected by physics or world boundaries.
+
+	Presets can be:
+	- x
+	- y
+	- size
+	- font
+	- fill
+*/
+function Text(_game, content, presets = {}) {
+	_game.object.init(this, "MobSin.Text", {tween: true});
+
+	// The actual text of this text object
+	this.content = content || "";
+
+	// The X and Y position. Text has no inherent width and height limit
+	this.pos = {
+		x: presets.x || 0,
+		y: presets.y || 0
+	};
+
+	// Text size, defaulted to 12 pixels
+	this.size = presets.size || 12;
+	// Text font, defaulted to Arial
+	this.font = presets.font || "Arial";
+	
+	// Text fill, defaulted to black
+	this.fill = presets.fill || "#000";
+
+	this._calculatePhysBounds = () => {};
+	this._render = (_ctx, offsets = {}) => {
+		_ctx.save();
+
+		_ctx.font = `${this.size}px ${this.font}`;
+		_ctx.fillStyle = this.fill;
+		_ctx.fillText(this.content, this.pos.x, this.pos.y);
+
+		_ctx.restore();
+	};
+}
+
+module.exports = (_game, content, presets) => new Text(_game, content, presets);
