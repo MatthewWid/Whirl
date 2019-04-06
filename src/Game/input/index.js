ef53c9089481dc7c622b7912387dd593dfd92875
MatthewWid <115mattey@gmail.com>
2019-04-06 21:06:43 +1100

    Add preparation placeholder code for mouse events.

--- src/Game/input/index.js
+++ src/Game/input/index.js
@@ -8,10 +8,16 @@ module.exports = (_game) => {
 	// Add setup functions to game input object
 	_game.input = {
 		setupKeyboard: keyboard.setup.bind(_game),
-		setKeyElement: keyboard.setKeyElement.bind(_game)
+		setKeyElement: keyboard.setKeyElement.bind(_game),
 	};
 
-	// If `input` is true then set up all input systems with defaults
+	_game.event.on("didInitObject", (e) => {
+		if (e.object._type === "MobSin.Viewport") {
+			// Attach mouse event to e.object if `input` is true
+		}
+	});
+
+	// If `input` is true then set up all input systems with default settings
 	if (!input) return;
 	_game.input.setKeyElement(inputElement)
 	_game.input.setupKeyboard();
