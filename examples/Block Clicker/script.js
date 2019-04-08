e67df334e13c25b6e165d660441b378d6fc1c449
MatthewWid <115mattey@gmail.com>
2019-04-08 19:06:58 +1000

    Remove hard-coded starting Sprites from 'Block Clicker' example.

--- examples/Block Clicker/script.js
+++ examples/Block Clicker/script.js
@@ -7,24 +7,9 @@ function setup(data) {
 
 	// Set up your game
 	// Create objects, initialise stage, viewport and camera, insert plugins, etc.
-	stage.child.add([
-		MobSin.Sprite(game, "block1", "#0F0", {
-			x: 50,
-			y: 50,
-			w: 50,
-			h: 50,
-			z: 4
-		}),
-		MobSin.Sprite(game, "block2", "#F00", {
-			x: 75,
-			y: 75,
-			w: 50,
-			h: 50,
-			z: 6
-		})
-	]);
 
-	const i = 3;
+	// Counter to ensure unique names
+	const i = 1;
 	// Listen for clicks on the viewport
 	viewport.event.on("mouseClick", (evt) => {
 		// When clicked create a new Sprite at the mouse position
@@ -39,18 +24,24 @@ function setup(data) {
 		);
 		// Make that Sprite fade in and grow bigger over the next .4 seconds
 		newSprite.tween({
+			// from
 			x: evt.pos.x,
 			y: evt.pos.y,
 			w: 0,
 			h: 0,
 			alpha: 0
 		}, {
+			// to
 			x: evt.pos.x - 25,
 			y: evt.pos.y - 25,
 			w: 50,
 			h: 50,
 			alpha: 1
-		}, 400, {
+		},
+		// ms
+		400, {
+			// modify the "bounds" object of the sprite
+			// set the easing function of the tween to out-quadratic
 			modify: "bounds",
 			easing: MobSin.easing.quadratic.out
 		});
