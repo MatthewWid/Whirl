5e8f3989bc927e61a2d1c044d3f295d6098c200b
MatthewWid <115mattey@gmail.com>
2019-04-08 18:17:21 +1000

    Add mouse relative and page position objects to 'mouseClick' viewport event. Fix bug where attempting to register mouse events on a non-viewport would not abort the process.

--- src/Game/input/mouse/index.js
+++ src/Game/input/mouse/index.js
@@ -7,13 +7,27 @@ function registerMouseElement(target) {
 	}
 	if (target._type !== "MobSin.Viewport" || !target.c) {
 		console.error("MobSin | Target element must be a MobSin.Viewport with a valid canvas element.");
+		return false;
 	}
 	const {c} = target;
 	this.object.attachSystem(target, {event: true});
 
 	c.addEventListener("click", (e) => {
+		// pageX/Y - Top left of content - affected by scrolling
+		// clientX/clientY - Top left of the content viewport
+		// screenX/Y - Top left of physical monitor screen
+
 		target.event.emit("mouseClick", {
-			rawEvent: e
+			rawEvent: e,
+			pos: {
+				// TODO: Include viewport and camera offsets in calculation
+				x: e.pageX - c.offsetLeft,
+				y: e.pageY - c.offsetTop
+			},
+			page: {
+				x: e.pageX,
+				y: e.pageY
+			}
 		});
 	});
 
