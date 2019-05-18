23495cadef43b0dd2f6c631e1f525a9c33dc7ce2
MatthewWid <115mattey@gmail.com>
2019-05-18 16:16:29 +1000

    Update .setup method to not necessarily require a canvas element to execute.

--- src/Game/setup/index.js
+++ src/Game/setup/index.js
@@ -1,28 +1,31 @@
 // Whirl.Game.setup
 
 function setup(presets) {
+	let setupData = {};
+
 	if (presets.canvas) {
 		const mainViewport = this.viewportManager.add("main", presets.canvas, Whirl.STAGE, Whirl.CAMERA, {
 			cW: presets.cW || presets.canvasWidth || canvas.width,
 			cH: presets.cH || presets.canvasHeight || canvas.height
 		});
-
-		if (presets.setup) {
-			this.event.onOnce("didSetup", presets.setup);
-		}
-		if (presets.update) {
-			this.event.on("willUpdate", presets.update);
-		}
-		this.event.emit("didSetup", {
-			game: this,
+		setupData = {
+			...setupData,
 			viewport: mainViewport,
 			canvas: mainViewport.c,
 			stage: mainViewport.activeStage,
-			camera: mainViewport.activeCamera,
-			setup: presets.setup || null,
-			update: presets.update || null
-		});
+			camera: mainViewport.activeCamera
+		};
 	}
+	if (presets.setup) {
+		this.event.onOnce("didSetup", presets.setup);
+		setupData.setup = presets.setup;
+	}
+	if (presets.update) {
+		this.event.on("willUpdate", presets.update);
+		setupData.update = presets.update;
+	}
+
+	this.event.emit("didSetup", {...setupData});
 
 	return this;
 }
