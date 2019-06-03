4e8f113ea973990028507494785456a4b6358034
MatthewWid <115mattey@gmail.com>
2019-06-03 15:30:48 +1000

    Remove Simple Setup example comments in favour of using the 'Getting Started' documentation section instead.

--- examples/Simple Setup/script.js
+++ examples/Simple Setup/script.js
@@ -1,17 +1,11 @@
 function update({game, frameCount}) {
-	// Run some code each update loop ...
+	
 }
 
 function setup({game, viewport, stage, camera}) {
-	// Set up your game
-	// Create objects, initialise stage, viewport and camera, insert plugins, etc.
+
 }
 
-// 1. Create a new game
-// 2. Call the 'setup' function when the "didSetup" event fires
-// 3. Call the 'update' function when the "willUpdate" event fires
-// 4. Set up the game with the given canvas and make it 400x400 pixels
-// 5. Start the game loop
 Whirl.Game()
 	.setup({
 		setup,
