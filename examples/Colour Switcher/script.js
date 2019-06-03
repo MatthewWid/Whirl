5121984c0e3e0cf8d3eae0aed0b9dec114234418
MatthewWid <115mattey@gmail.com>
2019-06-03 20:26:55 +1000

    Update Colour Switcher example to latest API changes.

--- examples/Colour Switcher/script.js
+++ examples/Colour Switcher/script.js
@@ -1,46 +1,5 @@
-document.getElementsByTagName("html")[0].style.backgroundColor = "#EEE";
-
-let wh = Whirl;
-
-let game = wh.Game();
-
-let v = game.viewportManager.add("vp", "#canvas", wh.STAGE, wh.CAMERA, {
-	cW: 400,
-	cH: 400
-});
-v.activeStage.child.add(
-	wh.Sprite(game, "background", "rgb(0, 0, 0)", {
-		w: 400,
-		h: 400
-	})
-);
-
-game.event.on("didUpdate", (data) => {
-	c.r = wh.math.stepTo(c.r, c.rT, c.step);
-	c.g = wh.math.stepTo(c.g, c.gT, c.step);
-	c.b = wh.math.stepTo(c.b, c.bT, c.step);
-
-	if (
-		wh.math.between(c.rT, c.r, c.rT, 3) &&
-		wh.math.between(c.gT, c.g, c.gT, 3) &&
-		wh.math.between(c.bT, c.b, c.bT, 3)
-	) {
-		setNewCol();
-	}
-});
-
-game.event.on("willRender", () => {
-	v.activeStage.child.getByName("background")[0].setFill(`rgb(${c.r}, ${c.g}, ${c.b})`);
-});
-
-// Set a new target colour
-function setNewCol() {
-	c.rT = wh.math.random(255);
-	c.gT = wh.math.random(255);
-	c.bT = wh.math.random(255);
-}
-
-let c = {
+const game = Whirl.Game();
+const c = {
 	r: 0,
 	g: 0,
 	b: 0,
@@ -49,21 +8,69 @@ let c = {
 	bT: 0,
 	step: 1
 };
-setNewCol();
-
-let debugText = {
+const debugText = {
 	padLeft: 20,
 	lineHeight: 10
-}
+};
+let bg;
 
-game.event.on("didRender", () => {
+
+// Debug text
+game.event.on("didRender", ({game}) => {
+	const v = game.viewport("main");
+
 	v.ctx.fillStyle = "rgb(255, 255, 255)";
 	v.ctx.fillText("Frame: " + game.frameCount, debugText.padLeft, 40 + debugText.lineHeight);
 
-	let keys = Object.keys(c);
+	const keys = Object.keys(c);
 	for (let i = 0; i < keys.length; i++) {
 		v.ctx.fillText(keys[i] + ": " + c[keys[i]], debugText.padLeft, 60 + debugText.lineHeight * i);
 	}
 });
 
-game.start();
+// Set a new target colour
+function setNewCol() {
+	c.rT = Whirl.math.random(255);
+	c.gT = Whirl.math.random(255);
+	c.bT = Whirl.math.random(255);
+}
+
+// Move the colour towards the target colour,
+// when it is reached get a new target colour
+function update({game}) {
+	c.r = Whirl.math.stepTo(c.r, c.rT, c.step);
+	c.g = Whirl.math.stepTo(c.g, c.gT, c.step);
+	c.b = Whirl.math.stepTo(c.b, c.bT, c.step);
+
+	game.stage("main").child.getByName("background")[0]
+		.setFill(`rgb(${c.r}, ${c.g}, ${c.b})`);
+
+	if (
+		Whirl.math.between(c.rT, c.r, c.rT, 3) &&
+		Whirl.math.between(c.gT, c.g, c.gT, 3) &&
+		Whirl.math.between(c.bT, c.b, c.bT, 3)
+	) {
+		setNewCol();
+	}
+}
+
+// Add new 'background' Sprite
+function setup({game, viewport, stage, camera}) {
+	setNewCol();
+
+	bg = Whirl.Sprite(game, "background", "rgb(0, 0, 0)", {
+		w: viewport.bounds.w,
+		h: viewport.bounds.h
+	});
+
+	stage.child.add(bg);
+}
+
+// Setup the game
+game.setup({
+	update,
+	setup,
+	canvas: "#canvas",
+	cW: 400,
+	cH: 400
+});
