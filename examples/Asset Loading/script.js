19e720f1d5b4cac655b13c0cca83ec8ceeebf74a
MatthewWid <115mattey@gmail.com>
2019-06-03 16:03:06 +1000

    Update examples/Asset Loading to use the latest API.

--- examples/Asset Loading/script.js
+++ examples/Asset Loading/script.js
@@ -1,43 +1,49 @@
-document.getElementsByTagName("html")[0].style.backgroundColor = "#FFF";
+function setup({game, viewport, stage, camera}) {
+	// We want to use pixel art, so let's turn off anti-aliasing
+	viewport.imageSmoothing = false;
 
-let wh = Whirl;
+	// Load an array of asset files
+	game.assetManager.load([
+		// Load an image file "playerSprite.png"
+		// called "player" from the "./img/" directory
+		{
+			name: "player",
+			type: "image",
+			src: "../_common/img/playerSprite.png"
+		},
+		// Load an audio file "8bit.mp3"
+		// called "bg_music" from the "./audio/" directory
+		{
+			name: "bg_music",
+			type: "audio",
+			src: "../_common/audio/8bit.mp3"
+		}
+	])
+	.event.on("didLoadAsset", ({asset, timeTaken}) => {
+		// If the loaded asset is our playerSprite.png image then
+		// create a Sprite and fill it with the image
+		// then add the Sprite to the world
+		if (asset.name === "player") {
+			const player = Whirl.Sprite(game, "player", asset, {
+				x: 100,
+				y: 100,
+				scale: 2
+			})
+				.anchor.center();
 
-let game = wh.Game();
+			stage.child.add(player);
+		}
+	})
+	.event.onOnce("didLoadAll", ({newAssets, timeTaken}) => {
+		console.log(newAssets);
+	});
+}
 
-// Load an array of assets
-game.assetManager.load([
-	// Load an image called "player" from the "./img/" directory
-	{
-		name: "player",
-		type: "image",
-		src: "../_common/img/playerSprite.png"
-	},
-	// Load an audio file called "bg_music" from the "./audio" directory
-	{
-		name: "bg_music",
-		type: "audio",
-		src: "../_common/audio/8bit.mp3"
-	}
-])
-.event.on("didLoadAsset", (data) => {
-	console.log(data);
-})
-.event.onOnce("didLoadAll", (data) => {
-	console.log(data);
-});
-
-let myVp = game.viewportManager.add("vp", "#canvas", wh.STAGE, wh.CAMERA, {
-	cW: 400,
-	cH: 400
-});
-
-let player = wh.Sprite(game, "player", game.assetManager.get("player"), {
-	x: 50,
-	y: 50,
-	w: 50,
-	h: 50,
-	anchor: {
-		x: .5,
-		y: .5
-	}
-});
+Whirl.Game()
+	.setup({
+		setup,
+		canvas: "#canvas",
+		cW: 400,
+		cH: 400
+	})
+	.start();
