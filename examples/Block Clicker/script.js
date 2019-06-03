9860ac65352908963cd9e1c4744eba7b5e7a364d
MatthewWid <115mattey@gmail.com>
2019-06-03 17:48:12 +1000

    Remove .start() and clean up unnecessary functions in Block Clicker example.

--- examples/Block Clicker/script.js
+++ examples/Block Clicker/script.js
@@ -1,45 +1,38 @@
-function update(data) {
-	// Run some code each update loop ...
-}
-
-function setup(data) {
-	let {game, viewport, stage, camera} = data;
-
-	// Set up your game
-	// Create objects, initialise stage, viewport and camera, insert plugins, etc.
-
+function setup({game, viewport, stage, camera}) {
 	// Counter to ensure unique names
 	const i = 1;
 	// Listen for clicks on the viewport
-	viewport.event.on("mouseClick", (evt) => {
+	viewport.event.on("mouseClick", ({pos}) => {
 		// When clicked create a new Sprite at the mouse position
 		const newSprite = Whirl.Sprite(
 			game,
 			`block${i}`,
-			Whirl.util.randRGB(),
+			Whirl.util.randRGB(), // random fill colour
 			{
-				x: evt.pos.x,
-				y: evt.pos.y
+				x: pos.x,
+				y: pos.y
 			}
 		);
 		// Make that Sprite fade in and grow bigger over the next .4 seconds
 		newSprite.tween({
 			// from
-			x: evt.pos.x,
-			y: evt.pos.y,
+			x: pos.x,
+			y: pos.y,
 			w: 0,
 			h: 0,
 			alpha: 0
 		}, {
 			// to
-			x: evt.pos.x - 25,
-			y: evt.pos.y - 25,
+			x: pos.x - 25,
+			y: pos.y - 25,
 			w: 50,
 			h: 50,
 			alpha: 1
 		},
 		// ms
-		400, {
+		400,
+		// options
+		{
 			// modify the "bounds" object of the sprite
 			// set the easing function of the tween to out-quadratic
 			modify: "bounds",
@@ -51,13 +44,10 @@ function setup(data) {
 	});
 }
 
-const game = Whirl.Game()
+Whirl.Game()
 	.setup({
 		setup,
-		update,
-
 		canvas: "#canvas",
 		cW: 600,
 		cH: 400
-	})
-	.start();
+	});
