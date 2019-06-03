aebe06ced9f38d01ef865b0855e69077dffc1b30
MatthewWid <115mattey@gmail.com>
2019-06-03 19:01:58 +1000

    Add example projects SASS->CSS compilation to webpack development configuration.

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,14 +1,35 @@
 const path = require("path");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
 	mode: "development",
 	entry: {
-		whirl: path.resolve(__dirname, "..", "./src/Whirl.js")
+		"./build/whirl": path.resolve(__dirname, "..", "./src/Whirl.js"),
+		"./examples/_common/styles/css/index": path.resolve(__dirname, "..", "./examples/_common/styles/scss/index.scss")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader"
+				]
+			}
+		]
 	},
 	output: {
-		path: path.resolve(__dirname, "..", "./build"),
+		path: path.resolve(__dirname, ".."),
 		filename: "[name].js"
 	},
+	plugins: [
+		new FixStyleOnlyEntriesPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+		})
+	],
 	watch: true
 };
 
