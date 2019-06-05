6b7e71c0d701a414a6bf9c18c67df0f00cb2222d
MatthewWid <115mattey@gmail.com>
2019-06-06 01:57:15 +1000

    Add 'deepmerge' package. Fix bug where if 'config.input' is not a boolean then the entire game configuration would be overwritten.

--- src/Game/config/index.js
+++ src/Game/config/index.js
@@ -1,12 +1,10 @@
 // Whirl.game.config
 
+const merge = require("deepmerge");
 const {defaultConfig} = require("../../constants");
 
 module.exports = (_game, presets) => {
-	_game.config = {
-		...defaultConfig,
-		...presets
-	};
+	_game.config = merge(defaultConfig, presets);
 
 	// If `input` is set to `true` then set `input` back to the proper defaults
 	if (_game.config.input === true) {
