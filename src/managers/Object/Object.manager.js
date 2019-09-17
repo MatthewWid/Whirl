71cd0d33670dfe3dfc400a1465f7207e78acb736
MatthewWid <115mattey@gmail.com>
2019-09-18 00:55:56 +1000

    Add '_viewports' field to ObjectManager that all instantiated viewports are added to. Add 'active' field to Base class. Update Base class constructor to set its own '_game' field instead of the ObjectManager implicitely setting it.

--- src/managers/Object/Object.manager.js
+++ src/managers/Object/Object.manager.js
@@ -1,9 +1,11 @@
 const Base = require("../../objects/Base/");
+const Viewport = require("../../objects/Viewport/");
 
 class ObjectManager {
 	_game;
 	_index = 0;
 	_store = [];
+	_viewports = [];
 
 	constructor(game) {
 		this._game = game;
@@ -14,9 +16,12 @@ class ObjectManager {
 			throw new Error("Whirl | Objects under a game instance must inherit from the Base object class.");
 		}
 
-		object._game = this._game;
 		object._id = this._index++;
 		this._store.push(object);
+
+		if (object instanceof Viewport._class) {
+			this._viewports.push(object);
+		}
 	}
 }
 
--- src/objects/Base/Base.js
+++ src/objects/Base/Base.js
@@ -1,12 +1,15 @@
 class Base {
 	_game;
 	_id;
+	active = true;
 	data = {};
 
 	constructor(game) {
 		if (!(game instanceof Whirl.Game._class)) {
 			throw new Error("Whirl | Game objects must be instantiated into a valid instance of a Whirl game.");
 		}
+
+		this._game = game;
 
 		game.object.add(this);
 	}
