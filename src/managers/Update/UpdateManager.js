da3de0e11d0c5c0055cecb0710b07cbbac3dea0d
MatthewWid <115mattey@gmail.com>
2019-09-22 00:33:35 +1000

    Add 'willStart', 'willStop', 'didStop', 'willUpdate' and 'didUpdate' events to Game instance.

--- src/managers/Update/UpdateManager.js
+++ src/managers/Update/UpdateManager.js
@@ -16,6 +16,13 @@ class UpdateManager {
 	}
 
 	_start = () => {
+		if (this.running) {
+			return this._game;
+		}
+
+		// Event: willStart
+		this._game.event.emit("willStart");
+
 		this._running = true;
 		this._update();
 
@@ -23,15 +30,34 @@ class UpdateManager {
 	}
 
 	_stop = () => {
+		// Event: willStop
+		this._game.event.emit("willStop");
+		
 		this._running = false;
 
 		return this._game;
 	}
 
 	_update = () => {
+		// Event: willUpdate
+		this._game.event.emit("willUpdate", {
+			frameCount: this._frameCount
+		});
+
 		this._frameCount++;
+
+		// Event: didUpdate
+		this._game.event.emit("didUpdate", {
+			frameCount: this._frameCount
+		});
+
 		if (this._running) {
 			requestAnimationFrame(this._update);
+		} else {
+			// Event: didStop
+			this._game.event.emit("didStop", {
+				frameCount: this._frameCount
+			});
 		}
 	}
 }
