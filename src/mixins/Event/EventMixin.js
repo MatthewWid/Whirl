1de88e36ebcf611292f9179cfe50909adc1d532f
MatthewWid <115mattey@gmail.com>
2019-09-22 00:43:59 +1000

    Fix bug in EventMixin where attempting to remove an event from a non-existent event pool on an object would cause a crash unnecessarily.

--- src/mixins/Event/EventMixin.js
+++ src/mixins/Event/EventMixin.js
@@ -44,6 +44,11 @@ class EventMixin extends Mixin {
 	}
 
 	remove(name, event) {
+		if (!this._events[name]) {
+			console.warn(`Whirl | Failed to remove event by ID from non-existent event pool "${name}".`);
+			return;
+		}
+
 		let _eId = -1;
 
 		// (string, Event)
