13a7d91d85f8809262b945ba58d379567a2d6776
MatthewWid <115mattey@gmail.com>
2019-09-25 23:26:22 +1000

    UpdateManager frame delta properties and events.
    - Add 'frameDelta', 'lastDelta', 'startTime', 'elapsedTime' and 'initTime' properties to Game.UpdateManager.
    - Add 'didStart' event to UpdateManager.
    - Update UpdateManager 'willStart' event to emit the time when the game was instantiated.
    - Update UpdateManager 'willUpdate' and 'didUpdate' events to emit the frameDelta, startTime and elapsedTime properties in their data objects.

--- src/managers/Update/UpdateManager.js
+++ src/managers/Update/UpdateManager.js
@@ -1,12 +1,19 @@
 const Manager = require("../Manager.js");
 
 class UpdateManager extends Manager {
-	_frameRate = 60;
-	_frameCount = 0;
-	_running = false;
+	_running = false; // Update loop running or not
+	_frameRate = 60; // Desired framerate per second
+	_frameCount = 0; // Total updates
+	_frameDelta = 0; // Time since last update
+	_lastDelta = 0; // Last delta time calculated
+	_startTime = 0; // Time at Game.start()
+	_elapsedTime = 0; // Time since Game.start()
+	_initTime; // Time at Game instantation
 
 	constructor(game) {
 		super(game);
+
+		this._initTime = performance.now();
 	}
 
 	get frameCount() {
@@ -22,10 +29,21 @@ class UpdateManager extends Manager {
 		}
 
 		// Event: willStart
-		this._game.event.emit("willStart");
+		this._game.event.emit("willStart", {
+			initTime: this._initTime,
+		});
 
 		this._running = true;
-		this._update();
+		this._startTime = performance.now();
+		this._elapsedTime = 0;
+
+		requestAnimationFrame(this._update);
+		
+		// Event: didStart
+		this._game.event.emit("didStart", {
+			initTime: this._initTime,
+			startTime: this._startTime,
+		});
 
 		return this._game;
 	}
@@ -39,17 +57,26 @@ class UpdateManager extends Manager {
 		return this._game;
 	}
 
-	_update = () => {
+	_update = (delta) => {
 		// Event: willUpdate
 		this._game.event.emit("willUpdate", {
-			frameCount: this._frameCount
+			frameCount: this._frameCount,
+			frameDelta: this._frameDelta,
+			startTime: this._startTime,
+			elapsedTime: this._elapsedTime,
 		});
 
 		this._frameCount++;
+		this._elapsedTime += delta - this._startTime;
+		this._frameDelta = delta - this._lastDelta;
+		this._lastDelta = delta;
 
 		// Event: didUpdate
 		this._game.event.emit("didUpdate", {
-			frameCount: this._frameCount
+			frameCount: this._frameCount,
+			frameDelta: this._frameDelta,
+			startTime: this._startTime,
+			elapsedTime: this._elapsedTime,
 		});
 
 		if (this._running) {
