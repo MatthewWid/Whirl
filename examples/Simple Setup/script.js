2f381b3b39e5617deb402b59eb5dc197bb5928c2
MatthewWid <115mattey@gmail.com>
2019-09-30 17:06:47 +1000

    Remove 2.0.0 examples.

--- examples/Simple Setup/script.js
+++ examples/Simple Setup/script.js
@@ -1,18 +1,5 @@
-function update({game, frameCount}) {
-	
-}
+const {Game} = Whirl;
 
-function setup({game, viewport, stage, camera}) {
-
-}
-
-Whirl.Game()
-	.setup({
-		setup,
-		update,
-
-		canvas: "#canvas",
-		cW: 400,
-		cH: 400
-	})
-	.start();
+const game = Game({
+	canvas: "#canvas",
+});
