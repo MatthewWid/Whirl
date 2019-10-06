e8e8d46f29186cba94b0416eeb00a058998b9559
MatthewWid <115mattey@gmail.com>
2019-10-06 13:30:50 +1100

    DOC: Whirl.mixins namespace and Mixin class documentation.

--- src/mixins/Mixin.js
+++ src/mixins/Mixin.js
@@ -1,12 +1,54 @@
+/**
+ * @classdesc
+ * The base mixin class that all other mixin classes extend from. Used to apply mixins to custom objects and create your own custom mixins.
+ * 
+ * Mixins are typically not instantiated directly, but by the use of the `Mixin.apply` method.
+ * 
+ * See {@link Whirl.mixins|the mixin namespace overview for common use cases and examples of creating and using mixins}.
+ * 
+ * @class Mixin
+ * @memberof Whirl.mixins
+ * @abstract
+ * 
+ * @param {object} [source] Source object that this mixin must be applied to.
+ */
 class Mixin {
+	/**
+	 * Property on the source object that the mixin will be namespaced under.
+	 * 
+	 * For example, a `_namespace` value of `event` will be attahced to the source object as `<Object>.event`.
+	 * 
+	 * @memberof Whirl.mixins.Mixin
+	 * @type {string}
+	 * @abstract
+	 */
 	static _namespace = null;
 
+	/**
+	 * The source object that this mixin instance is applied to.
+	 * 
+	 * @memberof Whirl.mixins.Mixin
+	 * @type {object}
+	 */
 	_source;
 
 	constructor(source) {
 		this._source = source;
 	}
 
+	/**
+	 * Apply mixins to a given object.
+	 * 
+	 * The source object must have an instance variable `mixins` that contains an array of mixin constructors to apply. Mixins are applied in the order that they are given.
+	 * 
+	 * Every class in the array must inherit from this `Mixin` class and override its static `_namespace` property with a valid string value.
+	 * 
+	 * After every mixin has been applied then the `mixin` property will be removed from the source object.
+	 * 
+	 * @method Whirl.mixins.Mixin.apply
+	 * 
+	 * @param {object} object Instance of an object to apply the set of mixins to.
+	 */
 	static apply(object) {
 		const {mixins} = object;
 
--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,3 +1,52 @@
+/**
+ * Contains constructors for mixins that are used as properties for various game objects and your own custom game objects.
+ * 
+ * When creating custom objects they must have an instance property called `mixins` that contains an array of mixin constructors. Then in your object constructor function you simply need to call `Mixin.apply(this);` to merge the given mixins into your object.
+ * 
+ * You can also create your own mixins using the base `Mixin` class that all other classes must inherit from. A custom mixin needs to `extend` `Mixin` and have a static property `_namespace` that represents the property name on the object that it will be attached to.
+ * 
+ * @namespace mixins
+ * @memberof Whirl
+ * 
+ * @example
+ * const {mixins: {Mixin, Event}} = Whirl;
+ * 
+ * // Add the `Event` mixin to `MyObject`
+ * class MyObject {
+ * 	mixins = [Event];
+ * 
+ * 	constructor() {
+ * 		Mixin.apply(this);
+ * 	}
+ * }
+ * 
+ * @example
+ * const {mixins: {Mixin}} = Whirl;
+ * 
+ * // Create a custom mixin `LogMixin`
+ * class LogMixin extends Mixin {
+ * 	static _namespace = "log"; // Attach under the `log` property
+ * 
+ * 	sayHi() {
+ * 		console.log("Hello world!");
+ * 	}
+ * }
+ * 
+ * // Create class `MyObject` and give it our `LogMixin`
+ * class MyObject {
+ * 	mixins = [LogMixin];
+ * 
+ * 	constructor() {
+ * 		Mixin.apply(this);
+ * 	}
+ * }
+ * 
+ * // Instantiate our `MyObject` class
+ * const obj = new MyObject();
+ * 
+ * // Call our `sayHi` method under our defined `log` namespace
+ * obj.log.sayHi(); // Output "Hello world" to console
+ */
 module.exports = {
 	Mixin: require("./Mixin.js"),
 	Event: require("./EventMixin/"),
