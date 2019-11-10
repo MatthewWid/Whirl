577322d563a4fbab44f868f5348fa34eebe3db4e
MatthewWid <115mattey@gmail.com>
2019-11-10 22:12:20 +1100

    Update DebugManager#warn and DebugManager#error methods to return the formatted error message as well as logging it. DOC: Add examples to DebugManager 'warn' and 'error' methods.

--- src/managers/DebugManager/DebugManager.js
+++ src/managers/DebugManager/DebugManager.js
@@ -24,17 +24,36 @@ class DebugManager extends Manager {
 	 * 
 	 * @param {string} text Warning message.
 	 * @param {string} [source="Generic"] Source of the warning.
+	 * @returns {string|null} Copy of the formatted message that was logged to console or `null` if the game is not in debug mode.
+	 * 
+	 * @example
+	 * game.config.set("debug", true); // Enable debug-mode
+	 * 
+	 * // > "Whirl | Generic | Something could go wrong!"
+	 * game.debug.warn("Something could go wrong!");
+	 * 
+	 * @example
+	 * game.config.set("debug", false); // Disable debug-mode
+	 * 
+	 * // Doesn't log, returns `null`
+	 * game.debug.warn("Something could go wrong!");
 	 */
 	warn(text, source = "Generic") {
 		if (this._game.config.get("debug")) {
-			console.warn(`Whirl | ${source} | ${text}`);
+			const msg = `Whirl | ${source} | ${text}`;
+
+			console.warn(msg);
+
+			return msg;
 		}
+		
+		return null;
 	}
 
 	/**
 	 * Logs a formatted error message to the console.
 	 * 
-	 * Throws a newly instantiated `Error` object that can be caught like any other error if need be.
+	 * Does not actually `throw` an `Error`, meaning program execution will not stop when this method is invoked. However, you can manually throw an error and assign its error message to the return value of this method to simulate that effect.
 	 * 
 	 * Executes regardless of whether the game is in debug mode or not.
 	 * 
@@ -42,9 +61,22 @@ class DebugManager extends Manager {
 	 * 
 	 * @param {string} text Error message.
 	 * @param {string} [source="Generic"] Source of the error.
+	 * @returns {string} Copy of the formatted message that was logged to console.
+	 * 
+	 * @example
+	 * // > "Whirl | Generic | Something went wrong!"
+	 * game.debug.error("Something went wrong!");
+	 * 
+	 * @example
+	 * // > "Whirl | SocketIO | Could not connect to the webserver."
+	 * game.debug.error("Could not connect to the webserver.", "SocketIO");
 	 */
 	error(text, source = "Generic") {
-		throw new Error(`Whirl | ${source} | ${text}`);
+		const msg = `Whirl | ${source} | ${text}`;
+
+		console.error(msg);
+
+		return msg;
 	}
 }
 
