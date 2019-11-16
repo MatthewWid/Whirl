56bd49f3ba78bb2523beab1036225cb0767399e7
MatthewWid <115mattey@gmail.com>
2019-11-16 16:40:10 +1100

    DOC: Add documentation for Texture and Colour classes.

--- src/objects/Colour/Colour.js
+++ src/objects/Colour/Colour.js
@@ -1,5 +1,17 @@
 const Texture = require("../Texture/");
 
+/**
+ * @classdesc
+ * Represents a colour texture that is a solid colour defined by an RGB value.
+ * 
+ * @class Colour
+ * @memberof Whirl
+ * @extends Whirl.Texture
+ * 
+ * @param {number} r Red intensity of this colour from `0` to `255`.
+ * @param {number} g Green intensity of this colour from `0` to `255`.
+ * @param {number} b Green intensity of this colour from `0` to `255`.
+ */
 class Colour extends Texture {
 	constructor(r, g, b) {
 		super();
--- src/objects/Texture/Texture.js
+++ src/objects/Texture/Texture.js
@@ -1,8 +1,35 @@
 const {Rectangle} = require("../../shapes");
 
+/**
+ * @classdesc
+ * Abstract class that all game textures must inherit from.
+ * 
+ * Textures are an applied image, colour, spritesheet, etc. that are used by other game objects to actually render something onto the canvas.
+ * 
+ * For most use cases you will never need to interact with the Texure class directly, but you may need to instantiate its relative subclasses for things such as Sprite rendering.
+ * 
+ * Texture data is immutable and should be treated as so. If you wish to change a texture you should instead create a new Texture.
+ * 
+ * @class Texture
+ * @memberof Whirl
+ * @abstract
+ */
 class Texture {
+	/**
+	 * Raw texture data of this texture.
+	 * 
+	 * @memberof Whirl.Texture#
+	 * @type {any}
+	 * @readonly
+	 */
 	_data;
 
+	/**
+	 * Get the raw texture data of this Texture.
+	 * 
+	 * @method Whirl.Texture#get
+	 * @returns {any}
+	 */
 	get() {
 		return this._data;
 	}
