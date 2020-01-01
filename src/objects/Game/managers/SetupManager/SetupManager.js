0e81b31375de089ce9ef04b6d09e0a2bf535a29e
MatthewWid <115mattey@gmail.com>
2020-01-01 22:38:48 +1100

    DOC: Add examples to SetupManager.

--- src/objects/Game/managers/SetupManager/SetupManager.js
+++ src/objects/Game/managers/SetupManager/SetupManager.js
@@ -8,16 +8,77 @@ const Viewport = require("../../../Viewport/");
  * During setup, performs the following operations in order:
  *
  * 1. If there is {@link Whirl.Game.ConfigManager#canvas|no default canvas set in the configuration}, will insert (append) a canvas element (with ID `whirl-canvas-default`) into {@link Whirl.Game.ConfigManager#root|the root element} (that defaults to the `<body>` element) and sets the `canvas` configuration variable to the ID selector of the created canvas.
- * 2. Creates a {@link Whirl.Stage|Stage} and {@link Whirl.Viewport|Viewport} object that contains it (both with dimensions `640x480`) and resizes the canvas to the size of the Viewport.
+ * 2. Creates a {@link Whirl.Stage|Stage} object and a {@link Whirl.Viewport|Viewport} that contains it, (both with dimensions `640x480`) and resizes the canvas to the size of the Viewport.
  * 3. Sets the {@link Whirl.Game.ConfigManager#setup|`setup` configuration variable} to `false` to block subsequent `setup` calls.
  * 4. Emits {@link Whirl.Game#event:didSetup|the didSetup event}.
  *
  * Once setup is complete, subsequent calls to the {@link Whirl.Game.SetupManager#setup|setup method} or the {@link Whirl.Game#start|Game#start method} will not start setup again.
  *
+ * Using the automated setup manager is optional. If you wish to create all of the foundational game objects from scratch, simply disable the `setup` property when making your game instance (`const game = Whirl.Game({setup: false})`).
+ *
  * @class SetupManager
  * @memberof Whirl.Game
+ *
+ * @example
+ * // Create a red Sprite and add it to the game world
+ * function setup({stage, viewport, game}) {
+ * 	const {Sprite, Colour} = game;
+ *
+ * 	stage.child.add([
+ * 		Sprite({
+ * 			bounds: Rectangle(200, 200, 50, 50),
+ * 			fill: Colour(200, 0, 0),
+ * 		}),
+ * 	]);
+ * }
+ *
+ * // Create a game instance, allowing automatic setup to take place
+ * const game = Whirl.Game()
+ * 	.event.once("didSetup", setup)
+ * 	.start();
+ *
+ * @example
+ * // Equivalent to the previous example, but doesn't use the `didSetup` event
+ *
+ * const game = {Sprite, Colour, setup: {stage}} = Whirl.Game()
+ * 	.setup.setup();
+ *
+ * stage.child.add([
+ * 	Sprite({
+ * 		bounds: Rectangle(200, 200, 50, 50),
+ * 		fill: Colour(200, 0, 0)
+ * 	}),
+ * ]);
+ *
+ * game.start();
  */
 class SetupManager extends Manager {
+	/**
+	 * Stage object created by the SetupManager during setup.
+	 *
+	 * This can be used to access the generated Stage directly, instead of getting it from the {@link Whirl.Game#event:didSetup|didSetup} event.
+	 *
+	 * Only stores the Stage created by the {@link Whirl.Game.SetupManager#setup|setup method}, not any custom stages you may create afterwards.
+	 *
+	 * @memberof Whirl.Game.SetupManager#
+	 * @type {Whirl.Stage}
+	 * @default null
+	 */
+	stage = null;
+
+	/**
+	 * Viewport object created by the SetupManager during setup.
+	 *
+	 * This can be used to access the generated Viewport directly, instead of getting it from the {@link Whirl.Game#event:didSetup|didSetup} event.
+	 *
+	 * Only stores the Viewport created by the {@link Whirl.Game.SetupManager#setup|setup method}, not any custom viewports you may create afterwards.
+	 *
+	 * @memberof Whirl.Game.SetupManager#
+	 * @type {Whirl.Viewport}
+	 * @default null
+	 */
+	viewport = null;
+
 	/**
 	 * Initiate the setup process for the game instance.
 	 *
@@ -62,12 +123,16 @@ class SetupManager extends Manager {
 			h: 480,
 		});
 
+		this.stage = stage;
+
 		const viewport = new Viewport(this._game, {
 			w: 640,
 			h: 480,
 			resize: true,
 			stage,
 		});
+
+		this.viewport = viewport;
 
 		config.set("setup", false);
 
