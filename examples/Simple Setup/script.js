f7a5a705c2280671fe1705f43264f088ae88ff81
MatthewWid <115mattey@gmail.com>
2020-01-01 22:48:03 +1100

    Update Simple Setup example to use the new SetupManager API.

--- examples/Simple Setup/script.js
+++ examples/Simple Setup/script.js
@@ -1,6 +1,28 @@
-const {Game} = Whirl;
+/**
+ * Simple boilerplate to get you up and tinkering.
+ * Start off by adding a coloured Sprite to your world.
+ */
+
+const {
+	Game,
+	geometry: {Rectangle, Point, Circle, Line},
+	math: {random}
+} = Whirl;
+
+function setup({stage, viewport, game}) {
+	const {Viewport, Stage, Container, Sprite, Colour} = game;
+
+	//
+}
+
+function update(data) {
+	//
+}
 
 const game = Game({
-	"canvas": "#canvas",
-	"debug": true,
-});
+	debug: true,
+	root: ".container",
+})
+	.event.once("didSetup", setup)
+	.event.on("didUpdate", update)
+	.start();
